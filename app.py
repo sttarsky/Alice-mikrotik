7f541968f431ff6f7df6a8f44e3f91643f6486fc
ilyashramko <ilya@skillbox>
2023-11-30 22:48:44 +0300

    update app.py

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def res():
     post_req = request.json
+    print(post_req)
     match post_req.get('session').get('skill_id'):
         case SKILL_ID:
             command = post_req.get('request')
             response_text = False, 'Команда недоступна'
             if not command.get('command'):
-                response_text = f'{mikrotik.connect()}'
+                response_text = mikrotik.connect()
             else:
                 response_text = guess.guesser(command.get('command'))
+            print(response_text)
             response = {
                 'response': {
                 'text': response_text[1],
